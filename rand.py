b10c0f1749abbaf0cdd31dcf56929aff01ad5d44
Sai Tarun Yellamraju <44906701+SaiTarun314@users.noreply.github.com>
2024-09-12 22:43:01 +0000

    Fixed rand.py

--- rand.py
+++ rand.py
@@ -6,7 +6,7 @@ import subprocess
 def random_array(arr):
     """Function to generate a random array"""
     shuffled_num = None
-    for i in enumerate(arr):
+    for i in range(len(arr)):
         shuffled_num = subprocess.run(
             ["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
         arr[i] = int(shuffled_num.stdout)
